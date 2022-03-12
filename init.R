e2ab8448ae06bf478cd1881572531e62a5f6479d
gn385x <giang.nglinh@gmail.com>
2022-03-11 22:07:28 -0800

    modify init

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes'))
+install.packages(c('dash', 'ggplot2', 'plotly', 'tidyverse', 'dplyr', 'purrr'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
